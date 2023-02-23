89411cc59b8c474e53e171431922611eb5bc4b64
Md. Khalid Hasan <shohag9438@gmail.com>
2023-02-23 20:25:02 +0600

    bug is fixed

--- water_quality_analysis/mapper.py
+++ water_quality_analysis/mapper.py
@@ -1,23 +1,24 @@
 import sys
-import re
-(id, station_name, parameter, value) = (None, None, None, 154656)
+(id, station_name, parameter, value) = (None, None, None, 454)
 count = 0
 for line in sys.stdin:
+   # print(line)
     words = line.strip().split(",")
+    count = 0
     for word in words:
         count = count+1
         if count == 1:
             id = word
-        elif count == 2:
+        elif count == 4:
             station_name = word
-        elif count == 3:
+        elif count == 15:
             parameter = word
-        elif count==4:
+        elif count==16:
             if parameter=='pH':
-             value = float(word)
-             print(id+station_name+" "+value)
+             #value = float(word)
+             print(id+" "+station_name+","+word)
             else:
                 break
-
+    #print(id + station_name)
 
 
--- water_quality_analysis/reducer.py
+++ water_quality_analysis/reducer.py
@@ -3,21 +3,40 @@ import sys
 (name, water_quality) = (None, None)
 (station_name, ph_value) = (None, 545)
 count = 0
-sum = 0
+sum = 0.0
 for line in sys.stdin:
-    (key, val) = line.split(", ")
-    if name!=key:
-        if(count>0):
-            averag_pH_value = sum/count
-            if(averag_pH_value>8):
-                print(key+" Base water")
-            elif averag_pH_value<6:
-                print(key+" Acidic water")
-        count = 0
-        (name, ph_value) = (key, float(val))
-        count = count+1
-        sum+=ph_value
-    else:
-        count+=1
-        sum+=float(val)
+    (key, val) = line.strip().split(",")
+    #print(key, val, name==key)
+    if name != key:
+        if count > 0:
+            average_pH_value = sum / count
 
+            if average_pH_value > 8 or average_pH_value < 6:
+                water_quality = "Bad"
+                # print(key + " " + str(averag_pH_value))
+            # elif average_pH_value < 6:
+            #     water_quality = "Bad"
+            #     # print(key + " " + str(averag_pH_value))
+            else:
+                water_quality = "Good"
+            print(name + " Water quality is" + water_quality)
+            count = 1;
+            (name, ph_value) = (key, float(val))
+            sum = ph_value
+           # print(name+" Water quality is"+water_quality)
+        elif count == 0:
+            # print(count)
+            count = 1
+            (name, ph_value) = (key, float(val))
+            sum += ph_value
+    else:
+        count += 1
+        sum += float(val)
+if name:
+    average_pH_value = sum / count
+    if average_pH_value > 8 or average_pH_value < 6:
+        water_quality = "Bad"
+    else:
+        water_quality = "Good"
+    print(name+ " Water quality is " + water_quality)
+    # print(name, str(average_pH_value))
