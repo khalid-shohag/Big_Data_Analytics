ec16a25d4317a42639bb9a308a84b7b2592e38de
Md. Khalid Hasan <shohag9438@gmail.com>
2023-03-28 17:13:29 +0600

    mrjob for triangle counting in a large graph

---
+++ Triangle_counting/TriangleCounting.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import sys
+
+#Hashing for 1-partition
+def modularHash(node, tableSize):
+    return node % tableSize
+nodes = []
+for i in range(4039):
+    nodes.append(i)
+
+list1 = []
+list = []
+for i in range(9):
+    list.append([])
+list.append(list1)
+for i in range(len(nodes)):
+    index = modularHash(nodes[i], 10)
+    list[index].append(nodes[i])
+
+
+
+def hashValue(vertex_list, vertex):
+    for i in range(len(vertex_list)):
+        if vertex_list[i].count(vertex) >= 1:
+            return i
+    return -1
+
+for line in sys.stdin:
+        (u, v) = line.strip().split(" ")
+        # print(u+" "+v)
+        u = int(u)
+        v = int(v)
+        # print(u, v)
+        indexU = hashValue(list, u)
+        indexV = hashValue(list, v)
+        # print(str(indexU)+" "+str(indexV))
+        partition = 50
+        for i in range(partition-1):
+            j = i+1
+            while j <= partition-1:
+                setOne = {i, j}
+                setTwo = {indexU, indexV}
+                if setTwo.issubset(setOne):
+                    print(str(i)+str(j)+", "+str(u)+" "+str(v))
+                    # fileOne.write(str(i)+" "+str(j)+". "+str(u)+" "+str(v)+"\n")
+                    # print(str(setTwo)+" "+str(setOne))
+                j+=1
+
+        if indexU!=indexV :
+            # print(str(indexU) + " " + str(indexV))
+            for i in range(partition-2):
+                j = i+1
+                while j <= partition - 2:
+                    k = j+1
+                    while k <= partition - 1:
+                        setOne = {i, j, k}
+                        setTwo = {indexU, indexV}
+                        if setTwo.issubset(setOne):
+                            print(str(i) + str(j) + str(k) + ", " + str(u) + " " + str(v))
+                            # fileOne.write(str(i) + " " + str(j) + " " + str(k) + ". " + str(u) + " " + str(v)+"\n")
+                            # print(str(setTwo) + " " + str(setOne))
+                        k += 1
+
+                    j += 1
+
+
---
+++ Triangle_counting/TriangleReducer.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+import sys
+
+
+def modularHash(node, tableSize):
+    return node % tableSize
+nodes = []
+for i in range(4039):
+    nodes.append(i)
+
+list1 = []
+list = []
+for i in range(9):
+    list.append([])
+list.append(list1)
+for i in range(len(nodes)):
+    index = modularHash(nodes[i], 10)
+    list[index].append(nodes[i])
+
+def hashValue(vertex_list, vertex):
+    for i in range(len(vertex_list)):
+        if vertex_list[i].count(vertex) >= 1:
+            return i
+    return -1
+
+triangle = 0.0
+hashing = list
+
+
+sameHashValue = 0
+
+edge_list = []
+(key, values) = (None, None)
+count = 0
+adj_list = []
+total_triangle = 0
+for i in range(81306):
+    adj_list.append([])
+
+partition = 50
+
+for line in sys.stdin:
+    # fileOne.write(line)
+    values = line.strip().split(", ")
+    v1 = values[0]
+    v2 = values[1]
+    if key != v1:
+        if count == 0:
+            key = v1
+            edge_list.append(v2)
+            (u, v) = v2.split(" ")
+            u = int(u)
+            v = int(v)
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+            count += 1
+        else:
+
+            #print("Edge: "+str(edge_list))
+
+            for i in range(len(edge_list)):
+                (u, v) = edge_list[i].split(" ")
+                u = int(u)
+                v = int(v)
+                setOne = {v}
+                setTwo = {u}
+
+                # Edge iterator algorithm for counting triangle
+                for x in adj_list[u]:
+                    setOne.add(x)
+                for x in adj_list[v]:
+                    setTwo.add(x)
+                
+                for x in setOne.intersection(setTwo):
+                    if u < v < x:
+                        if hashValue(hashing, x) == hashValue(hashing, u) == hashValue(hashing, v):
+                            total_triangle += float(1)/(partition-1)
+                        else:
+                            total_triangle += 1
+                        # print("Triangle: "+str(u)+str(v)+str(x)+" Total: "+str(total_triangle))
+                        # fileOne.write("Triangle: "+str(u)+str(v)+str(x)+" Total: "+str(total_triangle)+"\n")
+
+
+           
+            del edge_list[:]
+            del adj_list[:]
+
+           
+            for i in range(4039):
+                adj_list.append([])
+            count = 1
+            edge_list.append(v2)
+            (u, v) = v2.split(" ")
+            u = int(u)
+            v = int(v)
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+            key = v1
+            # print(total_triangle)
+
+    else:
+        edge_list.append(v2)
+        (u, v) = v2.split(" ")
+        u = int(u)
+        v = int(v)
+        adj_list[u].append(v)
+        adj_list[v].append(u)
+        count += 1
+
+if key:
+    # edge_list.append(v2)
+    # (u, v) = v2.split(" ")
+    # u = int(u)
+    # v = int(v)
+    # adj_list[u].append(v)
+    # adj_list[v].append(u)
+    #print("Edge: " + str(edge_list))
+
+    for i in range(len(edge_list)):
+        (u, v) = edge_list[i].split(" ")
+        u = int(u)
+        v = int(v)
+        setOne = {v}
+        setTwo = {u}
+
+        # Edge iterator algorithm for counting triangle
+        for x in adj_list[u]:
+            setOne.add(x)
+        for x in adj_list[v]:
+            setTwo.add(x)
+
+
+        for x in setOne.intersection(setTwo):
+            if u < v < x:
+                if hashValue(hashing, x) == hashValue(hashing, u) == hashValue(hashing, v):
+                    total_triangle += float(1) / (partition-1)
+                else:
+                    total_triangle += 1
+                # print("Triangle: " + str(u) + str(v) + str(x)+" Total: "+str(total_triangle))
+                # fileOne.write("Triangle: " + str(u) + str(v) + str(x) + " Total: " + str(total_triangle)+"\n")
+
+print(total_triangle)
+
